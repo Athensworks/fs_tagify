26030adabf28822c9a1274575915f22923c0c551
Ricky Chilcott <ricky@rbsavvy.com>
2013-03-22 11:33:48 -0400

    Base Node.js server with some functionality

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+
+function read_example(){
+  var fileName = "example.json";
+  var fs = require("fs");
+  
+  var data = fs.readFileSync(fileName, "utf8");
+
+  return data;
+};
+
+function parse_file(){
+  var json = JSON.parse(read_example());
+  return json;
+}
+
+app.get('/example.json', function(req, res){
+  var body = read_example();
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
+
+// app.get('/tags.js', function(req, res){
+//   res.setHeader('Content-Type', 'text/javascript');
+//   res.setHeader('Content-Length', javascript.length);
+//   res.end(javascript);
+// })
+
+app.get('/', function(req, res) {
+  res.render('tags.ejs');
+});
+
+app.get('/', function(req, res) {
+  res.render('index.jade');
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
